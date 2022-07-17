31c8812fd2391b06dbada0c09ee115c486c40eba
dimondrive <48328754+dimondrive@users.noreply.github.com>
2022-07-17 20:09:55 +0300

    Add files via upload

---
+++ To-Do-Native-JS-dimondri/script.js
@@ -0,0 +1,7 @@
+
+
+function myFunction() {
+    const h2=document.querySelector('.todo__body');
+    let html = "<p class='todo__tasks';>My new paragraphвыа.</p>";
+    h2.insertAdjacentHTML("afterend", html);
+  }
