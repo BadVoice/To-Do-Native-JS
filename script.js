9471c4f06bb3506422e8b5c29155a0fe502b8af2
dimondrive <48328754+dimondrive@users.noreply.github.com>
2022-07-18 09:47:53 +0300

    Изменил немного графику и код

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 
-
 function myFunction() {
-    const h2=document.querySelector('.todo__body');
-    let html = "<p class='todo__tasks';>My new paragraphвыа.</p>";
-    h2.insertAdjacentHTML("afterend", html);
+    const elemText=document.querySelector('.header__input');
+
+    if (!elemText.value.length || elemText.value.trim()==='') {
+      return;
+    }
+    else{
+      let getValue=elemText.value.trim();
+      getValue=getValue[0].toUpperCase()+getValue.slice(1);
+      let getTodoBody=document.querySelector('.todo__body');
+      let html = ` 
+    <div class="todo__tasks">
+    <p>${getValue}</p>
+    </div>`;
+    getTodoBody.insertAdjacentHTML("afterend", html);
+    elemText.value = '';
+    }
+    
   }
+ 
