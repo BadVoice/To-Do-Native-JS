4aea7bd925c5c233a9612cbf83a60a67db536ec4
Herman <smurovnemez@mail.ru>
2022-07-21 20:07:50 +0300

    add feature clean up tasks

--- script.js
+++ script.js
@@ -2,32 +2,38 @@ const taskAddition = () => {
     const todoBody = document.querySelector('.todo__body'),
         todoTask = document.querySelector('.body__task'),
         headerInput = document.querySelector('.header__input'),
-        headerBtn = document.querySelector('.header__btn');
+        headerBtn = document.querySelector('.header__btn'),
+        clearTasks = document.getElementById('clear')
 
     let getValue
+
+
+    headerBtn.addEventListener('click', (e) => {
+        e.preventDefault()
+        getValue = headerInput.value;
+        getValue.trim()
+        if (!getValue.length || getValue.trim() === '') {
+            return
+        } else {
+            additionTask();
+            headerInput.value = '';
+        }
+    })
 
     const additionTask = () => {
         let task = document.createElement('div')
         task.classList.add('body__task')
 
         task.innerHTML = `
-          <p>${getValue}</p>
-      `
-
+        <p class = 'task__descr'>${getValue}</p>
+        <button class = 'task__delete'> Delete</button>
+    `
         todoBody.append(task)
+
+        clearTasks.addEventListener('click', () => {
+            task.remove()
+        })
     }
-
-    headerBtn.addEventListener('click', (e) => {
-        e.preventDefault()
-        getValue = headerInput.value;
-        getValue.trim()
-        if (getValue.trim() !== getValue.length) {
-            additionTask()
-        } else {
-            return false
-        }
-
-    })
 
 
 }
